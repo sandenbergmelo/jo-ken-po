5539dfc3bfd36494df449205fa1af312247d6d5e
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2022-10-10 13:26:01 -0300

    Remove emoji module

--- jokenpo.py
+++ jokenpo.py
@@ -1,64 +1,61 @@
 from random import randint
 from time import sleep
-from emoji import emojize as emoji
 from os import system, name
 
-itens = (emoji('Pedra ✊'),
-		emoji('Papel 🖐'),
-		emoji('Tesoura ✌'))
+itens = ('Pedra ✊', 'Papel 🖐', 'Tesoura ✌')
 
 while True:
-	computador = randint(0, 2)
+    computador = randint(0, 2)
 
-	print('-=' * 10)
-	print('Jo Ken Po!'.center(20))
-	print('-=' * 10)
+    print('-=' * 10)
+    print('Jo Ken Po!'.center(20))
+    print('-=' * 10)
 
-	jogador = int(input('''O que vai jogar?
+    jogador = int(input('''O que vai jogar?
 [0] Pedra
 [1] Papel
 [2] Tesoura
 Opção: '''))
 
-	if 0 <= jogador <= 2:
+    if 0 <= jogador <= 2:
 
-		print('JO')
-		sleep(.7)
-		print('KEN')
-		sleep(.7)
-		print('PÔ!!!')
+        print('JO')
+        sleep(.7)
+        print('KEN')
+        sleep(.7)
+        print('PÔ!!!')
 
-		print('-=' * 12)
-		print(f'Jogador jogou {itens[jogador]}')
-		print(f'Computador jogou {itens[computador]}')
-		print('-=' * 12)
+        print('-=' * 12)
+        print(f'Jogador jogou {itens[jogador]}')
+        print(f'Computador jogou {itens[computador]}')
+        print('-=' * 12)
 
-		if computador == 0: # Computador escolheu PEDRA
-			if jogador == 0:
-				print('EMPATE')
-			elif jogador == 1:
-				print('JOGADOR VENCEU')
-			elif jogador == 2:
-				print('COMPUTADOR VENCEU')
+        if computador == 0:  # Computador escolheu PEDRA
+            if jogador == 0:
+                print('EMPATE')
+            elif jogador == 1:
+                print('JOGADOR VENCEU')
+            elif jogador == 2:
+                print('COMPUTADOR VENCEU')
 
-		elif computador == 1: # Computador escolheu PAPEL
-			if jogador == 0:
-				print('COMPUTADOR VENCEU')
-			elif jogador == 1:
-				print('EMPATE')
-			elif jogador == 2:
-				print('JOGADOR VENCEU')
+        elif computador == 1:  # Computador escolheu PAPEL
+            if jogador == 0:
+                print('COMPUTADOR VENCEU')
+            elif jogador == 1:
+                print('EMPATE')
+            elif jogador == 2:
+                print('JOGADOR VENCEU')
 
-		elif computador == 2: # Computador escolheu TESOURA
-			if jogador == 0:
-				print('JOGADOR VENCEU')
-			elif jogador == 1:
-				print('COMPUTADOR VENCEU')
-			elif jogador == 2:
-				print('EMPATE')
+        elif computador == 2:  # Computador escolheu TESOURA
+            if jogador == 0:
+                print('JOGADOR VENCEU')
+            elif jogador == 1:
+                print('COMPUTADOR VENCEU')
+            elif jogador == 2:
+                print('EMPATE')
 
-		input('\nPressione ENTER para jogar de novo ')
-		system('cls' if name == 'nt' else 'clear')
-	else:
-		print('Opção INVÁLIDA')
-		input('\nPressione ENTER para jogar de novo ')
+        input('\nPressione ENTER para jogar de novo ')
+        system('cls' if name == 'nt' else 'clear')
+    else:
+        print('Opção INVÁLIDA')
+        input('\nPressione ENTER para jogar de novo ')
